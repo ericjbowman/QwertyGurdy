76a702ab99bfb92f576f921be5a60f4d291d323e
ericbowman <ericjbowman@gmail.com>
2019-09-26 16:39:26 -0400

    Added volume, pitch, and reverb controls for keys

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -3,6 +3,7 @@ const config = require('./config.js')
 const store = require('./store.js')
 const api = require('./api.js')
 const indexUploads = require('./templates/index-uploads.handlebars')
+// const getFormFields = require('../../lib/get-form-fields.js')
 
 let loop = 'loop1'
 
@@ -43,33 +44,63 @@ const kits = {
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillKick1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillPerc2.wav`,
-        volume: 0.5
-        // reverb: {
-        //   wet: 0.5,
-        //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
-        // }
-        // Reverb slows everything down interfering with sound rendering
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
       }
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillSnare4.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillHat1.wav`,
-        volume: 0.4 }
+        volume: 0.4,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillPerc1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/MrBill/MrBillSnare3.wav`,
-        volume: 0.4 }
+        volume: 0.4,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     )
   },
   // PointPoint: {
@@ -127,27 +158,41 @@ const kits = {
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalLoop1.wav`,
         loop: true,
         volume: 0.5,
+        detune: 0,
         rate: 1
       }),
     loop2: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalLoop2.wav`,
         loop: true,
         volume: 0.5,
+        detune: 0,
         rate: 1
       }),
     loop3: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalLoop3.wav`,
         loop: true,
         volume: 0.5,
+        detune: 0,
         rate: 1
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalKick1.wav`,
-        volume: 0.6 }
+        volume: 0.6,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalTom.wav`,
-        volume: 0.5
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
         // reverb: {
         //   wet: 0.5,
         //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
@@ -157,19 +202,42 @@ const kits = {
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalSnare1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalHat1.wav`,
-        volume: 0.3 }
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalPerc3.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Pascaal/PascaalPerc2.wav`,
-        volume: 0.3
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
       }
     )
   },
@@ -194,33 +262,62 @@ const kits = {
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonKick1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonTom1.wav`,
-        volume: 0.5
-        // reverb: {
-        //   wet: 0.5,
-        //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
-        // }
-        // Reverb slows everything down interfering with sound rendering
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
       }
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonSnare1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonHat1.wav`,
-        volume: 0.3 }
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonPerc1.wav`,
-        volume: 0.3 }
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonPerc2.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     )
   },
   Oliver: {
@@ -244,11 +341,22 @@ const kits = {
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverKick1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverTom1.wav`,
-        volume: 0.5
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
         // reverb: {
         //   wet: 0.5,
         //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
@@ -258,19 +366,43 @@ const kits = {
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverSnare1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverHat1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverHat2.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Oliver/OliverPerc2.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     )
   },
   'Torres-Rengifo': {
@@ -294,11 +426,22 @@ const kits = {
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoKick1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoTom1.wav`,
-        volume: 0.5
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
         // reverb: {
         //   wet: 0.5,
         //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
@@ -308,19 +451,43 @@ const kits = {
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoPerc1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoHat1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoRim1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Torres-Rengifo/Torres-RengifoPerc2.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     )
   },
   Custom: {
@@ -344,11 +511,22 @@ const kits = {
       }),
     s: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonKick1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     d: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonTom1.wav`,
-        volume: 0.5
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
         // reverb: {
         //   wet: 0.5,
         //   impulse: 'https://bowmansbucket.s3.amazonaws.com/CementBlocks1.wav'
@@ -358,19 +536,43 @@ const kits = {
     ),
     f: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonSnare1.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     j: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonHat1.wav`,
-        volume: 0.3 }
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     k: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonPerc1.wav`,
-        volume: 0.3 }
+        volume: 0.3,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     ),
     l: new Wad(
       { source: `https://bowmansbucket.s3.amazonaws.com/Tennyson/TennysonPerc2.wav`,
-        volume: 0.5 }
+        volume: 0.5,
+        detune: 0,
+        reverb: {
+          wet: 0,
+          impulse: 'public/CementBlocks1.wav'
+        }
+      }
     )
   }
 }
@@ -505,6 +707,44 @@ const onChangeDetune = event => {
   }
 }
 
+const onChangeKeyVolume = (event, key) => {
+  kits[producer][key] = new Wad({
+    source: kits[producer][key].source,
+    detune: kits[producer][key].detune,
+    reverb: {
+      wet: kits[producer][key].reverb.wet,
+      impulse: 'public/CementBlocks1.wav'
+    },
+    volume: event.target.valueAsNumber
+  })
+}
+
+const onChangeKeyDetune = (event, key) => {
+  // kits[producer][key].setDetune(event.target.valueAsNumber)
+  // console.log('key detune', kits[producer][key].detune)
+  kits[producer][key] = new Wad({
+    source: kits[producer][key].source,
+    volume: kits[producer][key].volume,
+    reverb: {
+      wet: kits[producer][key].reverb.wet,
+      impulse: 'public/CementBlocks1.wav'
+    },
+    detune: event.target.valueAsNumber
+  })
+}
+
+const onChangeKeyReverb = (event, key) => {
+  kits[producer][key] = new Wad({
+    source: kits[producer][key].source,
+    volume: kits[producer][key].volume,
+    detune: kits[producer][key].detune,
+    reverb: {
+      wet: event.target.valueAsNumber,
+      impulse: 'public/CementBlocks1.wav'
+    }
+  })
+}
+
 const onSelectLoop = event => {
   switch (event.target.value) {
     case 'Loop 1': loop = 'loop1'
@@ -594,6 +834,30 @@ const addHandlers = () => {
   $('#Custom').on('click', () => onClickProducer('Custom'))
   $('#volume').on('change', onChangeVolume)
   $('#detune').on('change', onChangeDetune)
+  $('#s-volume').on('change', (event) => onChangeKeyVolume(event, 's'))
+  $('#s-detune').on('change', (event) => onChangeKeyDetune(event, 's'))
+  $('#s-reverb').on('change', (event) => onChangeKeyReverb(event, 's'))
+
+  $('#d-volume').on('change', (event) => onChangeKeyVolume(event, 'd'))
+  $('#d-detune').on('change', (event) => onChangeKeyDetune(event, 'd'))
+  $('#d-reverb').on('change', (event) => onChangeKeyReverb(event, 'd'))
+
+  $('#f-volume').on('change', (event) => onChangeKeyVolume(event, 'f'))
+  $('#f-detune').on('change', (event) => onChangeKeyDetune(event, 'f'))
+  $('#f-reverb').on('change', (event) => onChangeKeyReverb(event, 'f'))
+
+  $('#j-volume').on('change', (event) => onChangeKeyVolume(event, 'j'))
+  $('#j-detune').on('change', (event) => onChangeKeyDetune(event, 'j'))
+  $('#j-reverb').on('change', (event) => onChangeKeyReverb(event, 'j'))
+
+  $('#k-volume').on('change', (event) => onChangeKeyVolume(event, 'k'))
+  $('#k-detune').on('change', (event) => onChangeKeyDetune(event, 'k'))
+  $('#k-reverb').on('change', (event) => onChangeKeyReverb(event, 'k'))
+
+  $('#l-volume').on('change', (event) => onChangeKeyVolume(event, 'l'))
+  $('#l-detune').on('change', (event) => onChangeKeyDetune(event, 'l'))
+  $('#l-reverb').on('change', (event) => onChangeKeyReverb(event, 'l'))
+
   $('.loops').on('change', onSelectLoop)
   $('#handlebar-uploads').on('change', '.custom-select', onSelectCustomLoop)
   $('#s').on('click', () => kits[producer].s.play())
@@ -610,9 +874,11 @@ const addHandlers = () => {
     // const formData = {
     //   file
     // }
+    // const formFields = getFormFields(this)
+    // console.log('form fields', formFields)
     const formData = new FormData(event.target)
-    console.log('event target isLoop', event.target.value)
-    console.log('formData', formData)
+    // console.log('event target isLoop', event.target.value)
+    console.log('formData', formData.values())
 
     // let type = ''
     // if ($('#is-loop').prop('checked', true)) {
