ecf6b2ebd11320a52ccd13f33c47d507ebc7b94d
ericbowman <ericjbowman@gmail.com>
2019-09-29 22:04:57 -0400

    Mobile modal triggered if user device is mobile

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -798,7 +798,25 @@ const uploadOneShot = (key, event) => {
     .catch(() => alert('failure'))
 }
 
+const detectmob = () => {
+  if (navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
 const addHandlers = () => {
+  if (detectmob()) {
+    $('#mobile-modal').modal('show')
+  }
   const httpRequest = new XMLHttpRequest()
   httpRequest.open('GET', 'https://bowmansbucket.s3.amazonaws.com/Oliver/OliverLoop3.wav')
   httpRequest.addEventListener('load', e => $('.loader').hide())
